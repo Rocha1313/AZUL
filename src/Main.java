7cfda05e5ca366b1dd1b537b7b0028cb37417258
Costa1515 <116725667+Costa1515@users.noreply.github.com>
2022-12-20 16:19:17 +0000

    sou lindo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("O Costa fica para último mas tambem é lindo.");
     }
 }
