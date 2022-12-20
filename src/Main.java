53a38e0edece55102d0722cae859e3f69ed4bee1
rafaSpdg <117362646+rafaSpdg@users.noreply.github.com>
2022-12-20 16:00:55 +0000

    Commit Rafa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Rafa é o lindo");
+        System.out.println("Mas o rocha tambem.");
     }
 }
